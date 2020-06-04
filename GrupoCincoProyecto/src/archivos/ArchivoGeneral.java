e0c3075df480298f7b105c2966e6a8dc74fef67f
Diego Merino <diegopaul7@icloud.com>
2020-06-04 12:03:43 -0500

    archivosDAT

---
+++ GrupoCincoProyecto/src/archivos/ArchivoGeneral.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package archivos;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author diegomerino
+ */
+public class ArchivoGeneral {
+
+    public static void EscribirDAT(ArrayList ArrayObjetos, String fichero) throws FileNotFoundException, IOException {
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichero, false));
+        for (Object objAux : ArrayObjetos) {
+            oos.writeObject(objAux);
+        }
+        oos.close();
+    }
+
+    public static void LeerDAT(ArrayList ArrayObjetos, String fichero) {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichero));
+            Object aux = ois.readObject();
+            while (aux != null) {
+                if (aux instanceof Object) {
+                    ArrayObjetos.add(aux);
+                }
+                aux = ois.readObject();
+            }
+            ois.close();
+        } catch (EOFException e1) {
+        } catch (Exception e2) {
+            e2.printStackTrace();
+        }
+    }
+}
