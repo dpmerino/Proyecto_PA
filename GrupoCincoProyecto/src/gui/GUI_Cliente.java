6c763414cca85965e72bf3005bf83a946993831a
Jean Chuquimarca <jeanchuquimarca12@gmail.com>
2020-06-05 03:47:09 -0500

    jean

--- GrupoCincoProyecto/src/gui/GUI_Cliente.java
+++ GrupoCincoProyecto/src/gui/GUI_Cliente.java
@@ -242,7 +242,7 @@ public class GUI_Cliente extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         Local objLocal = objLogLoc.BuscarLocal(ArrayFarmacias, this.jComboFarmacias.getSelectedItem().toString());
-        Inventario objInv = objLogI
+        Inventario objInv ;
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
