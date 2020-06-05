b654352298276732cde6cbb7268a8268fd022307
Diego Merino <diegopaul7@icloud.com>
2020-06-04 21:46:28 -0500

    Revert "Merge branch 'master' of https://github.com/dpmerino/Proyecto_PA"
    This reverts commit fefc838652d1ef61d330110f6d49af70e1a31514, reversing
    changes made to fce495d2abe1c79d45e90c4de5bd1de106e50a04.

--- GrupoCincoProyecto/src/archivos/ArchivoGeneral.java
+++ GrupoCincoProyecto/src/archivos/ArchivoGeneral.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 
 /**
  *
+ * @author diegomerino
  */
 public class ArchivoGeneral {
 
--- GrupoCincoProyecto/src/logica/LogicaGerente.java
+++ GrupoCincoProyecto/src/logica/LogicaGerente.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 
 /**
  *
+ * @author diegomerino
  */
 public class LogicaGerente {
     static String fichero = "archivos/gerentes.dat";
