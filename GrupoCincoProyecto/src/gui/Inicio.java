2232eb90b8f21d0c6f3f025cb502db75c5c3e668
Jean Chuquimarca <jeanchuquimarca12@gmail.com>
2020-06-05 06:00:57 -0500

    jean

--- GrupoCincoProyecto/src/gui/Inicio.java
+++ GrupoCincoProyecto/src/gui/Inicio.java
@@ -28,14 +28,16 @@ public class Inicio extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jButtonCliente = new javax.swing.JButton();
-        jButtonEmpleado = new javax.swing.JButton();
+        jButtonBodeguero = new javax.swing.JButton();
         jButtonSalir = new javax.swing.JButton();
         jButtonAdmin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel1.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
         jLabel1.setText("Seleccione como desea ingresar");
 
+        jButtonCliente.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jButtonCliente.setText("Cliente");
         jButtonCliente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -43,13 +45,15 @@ public class Inicio extends javax.swing.JFrame {
             }
         });
 
-        jButtonEmpleado.setText("Empleado");
-        jButtonEmpleado.addActionListener(new java.awt.event.ActionListener() {
+        jButtonBodeguero.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jButtonBodeguero.setText("Bodeguero");
+        jButtonBodeguero.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonEmpleadoActionPerformed(evt);
+                jButtonBodegueroActionPerformed(evt);
             }
         });
 
+        jButtonSalir.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jButtonSalir.setText("Salir");
         jButtonSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -57,6 +61,7 @@ public class Inicio extends javax.swing.JFrame {
             }
         });
 
+        jButtonAdmin.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         jButtonAdmin.setText("Administrador");
         jButtonAdmin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -68,40 +73,37 @@ public class Inicio extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(89, 89, 89)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(58, 58, 58)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addComponent(jButtonCliente)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addComponent(jButtonAdmin)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                .addComponent(jButtonBodeguero)
+                .addGap(49, 49, 49))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButtonAdmin)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(202, 202, 202)
                         .addComponent(jButtonSalir))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButtonCliente)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 96, Short.MAX_VALUE)
-                        .addComponent(jButtonEmpleado)))
-                .addGap(81, 81, 81))
+                        .addGap(98, 98, 98)
+                        .addComponent(jLabel1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(21, 21, 21)
+                .addGap(39, 39, 39)
                 .addComponent(jLabel1)
-                .addGap(84, 84, 84)
+                .addGap(66, 66, 66)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonCliente)
-                    .addComponent(jButtonEmpleado))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 105, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButtonSalir)
-                        .addGap(20, 20, 20))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButtonAdmin)
-                        .addGap(30, 30, 30))))
+                    .addComponent(jButtonBodeguero)
+                    .addComponent(jButtonAdmin))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 81, Short.MAX_VALUE)
+                .addComponent(jButtonSalir)
+                .addGap(43, 43, 43))
         );
 
         pack();
@@ -118,9 +120,11 @@ public class Inicio extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jButtonSalirActionPerformed
 
-    private void jButtonEmpleadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEmpleadoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButtonEmpleadoActionPerformed
+    private void jButtonBodegueroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBodegueroActionPerformed
+        new GUI_Bodeguero().setVisible(true);
+        this.dispose();
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButtonBodegueroActionPerformed
 
     private void jButtonAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAdminActionPerformed
         new CrearLocal().setVisible(true);
@@ -165,8 +169,8 @@ public class Inicio extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonAdmin;
+    private javax.swing.JButton jButtonBodeguero;
     private javax.swing.JButton jButtonCliente;
-    private javax.swing.JButton jButtonEmpleado;
     private javax.swing.JButton jButtonSalir;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
