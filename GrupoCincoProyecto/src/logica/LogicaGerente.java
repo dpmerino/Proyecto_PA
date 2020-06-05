8897172b2c2395a6303e762e6b6c7c9a7e944ed7
Jean Chuquimarca <jeanchuquimarca12@gmail.com>
2020-06-05 03:08:51 -0500

    jean

--- GrupoCincoProyecto/src/logica/LogicaGerente.java
+++ GrupoCincoProyecto/src/logica/LogicaGerente.java
@@ -8,6 +8,8 @@ package logica;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -22,5 +24,15 @@ public class LogicaGerente {
     
     public static void LeerGerenteDAT (ArrayList ArrayObjetos) throws IOException, FileNotFoundException, ClassNotFoundException{
         archivos.ArchivoGeneral.LeerDAT(ArrayObjetos, fichero);
+    }
+    public boolean mailValido(String email) {       
+       Pattern pat = 
+       Pattern.compile("^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+       Matcher mat = pat.matcher(email);
+       if(mat.find()){
+          return true;
+       }else{
+          return false;
+     }
     }
 }
