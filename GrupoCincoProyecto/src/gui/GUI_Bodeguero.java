04b64dde6c44b038f4f8eb521a2c72b00a200049
krcondolo <kevincondolo@gmail.com>
2020-06-05 01:43:52 -0500

    .
    .

--- GrupoCincoProyecto/src/gui/GUI_Bodeguero.java
+++ GrupoCincoProyecto/src/gui/GUI_Bodeguero.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import logica.LogicaLocal;
+import clases.Producto;
 
 /**
  *
@@ -25,9 +26,11 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
      */
     
     static LogicaLocal ObjLogLoc = new LogicaLocal();
+    
     ArrayList<Local> ArrayLocales = new ArrayList<Local>();
     static ArrayList ArrayInventario = new ArrayList<Inventario>();
     static boolean HayLocales;
+    
     public GUI_Bodeguero() {
         initComponents();
     }
@@ -54,6 +57,9 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jTextCanti = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jTextCedula = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -82,8 +88,8 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
         getContentPane().add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 210, 80, -1));
 
         jLabel7.setText("Cantidad");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 90, -1, -1));
-        getContentPane().add(jTextCanti, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 90, 70, 20));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 120, -1, -1));
+        getContentPane().add(jTextCanti, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 120, 70, 20));
 
         jButton1.setText("Agregar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -93,10 +99,20 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
         });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 240, -1, -1));
 
+        jLabel8.setText("Cedula");
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 60, -1, -1));
+        getContentPane().add(jTextCedula, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 60, 90, -1));
+
+        jButton2.setText("Ingresar");
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 60, -1, -1));
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+      Local ObjLocal = ObjLogLoc.BuscarConBodeguero(ArrayLocales, jTextCedula.getText());
+      Producto ObjProd = new Producto(WIDTH, nombre, ERROR, vencimiento);
+      
       
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -144,6 +160,7 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -151,7 +168,9 @@ public class GUI_Bodeguero extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JTextField jTextCanti;
+    private javax.swing.JTextField jTextCedula;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
